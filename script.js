3cb625f55229ff424f4b20a8cd0a48e9d39a4c55
Shakvilla <abdulshakuraclement@yahoo.com>
2021-06-02 22:35:00 +0000

    now adding quote to html dynamically

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+const quoteConainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote')
+
 // let apiQuotes = [];
 
 function newQuote() {
 
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote)
+    // console.log(quote)
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text
+
 }
 
 // async function getQuotes() {
