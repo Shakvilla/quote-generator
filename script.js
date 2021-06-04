7cd1d589dcf1229ae23d28e67c0801e2040caf12
Shakvilla <abdulshakuraclement@yahoo.com>
2021-06-04 15:55:54 +0000

    finished the code

--- script.js
+++ script.js
@@ -1,42 +1,75 @@
-const quoteConainer = document.getElementById('quote-container');
+const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote')
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader')
 
-// let apiQuotes = [];
+let apiQuotes = [];
 
+//show loader
+function loading() {
+
+    loader.hidden = false;
+    quoteContainer.hidden = true
+}
+
+//hide loader
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true
+}
+
+// function to randomly generate new quote
 function newQuote() {
+    loading();
 
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    // console.log(quote)
-    authorText.textContent = quote.author;
-    quoteText.textContent = quote.text
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+    (!quote.author) ? authorText.textContent = "Unknown" : authorText.textContent = quote.author;
+
+
+    (quote.text.length > 50) ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote');
+    quoteText.textContent = quote.text;
+
+    complete()
 
 }
+
+// share quote on twitter
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank')
+}
+
+
+//Add Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 newQuote()
 
-// async function getQuotes() {
 
-//     const apiUrl = 'https://type.fit/api/quotes'
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes'
 
-//     try {
+    try {
 
-//         const response = await fetch(apiUrl);
-//         apiQuotes = await response.json();
-//         // console.table(apiQuotes)
-//         newQuote();
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        // console.table(apiQuotes)
+        newQuote();
 
-//     } catch (error) {
-
-
-//         console.table(error)
+    } catch (error) {
 
 
-//     }
-
-// }
+        console.table(error)
 
 
-// getQuotes()
+    }
 
+}
+
+
+getQuotes()
